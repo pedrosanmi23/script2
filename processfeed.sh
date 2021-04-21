eea9219eb55206365a82fcdc7f2c9b780f11cc0f
Jose-zapataguil <josezapata4d@gmail.com>
2021-04-21 11:54:35 +0000

    Ahora genera un fichero HTML

--- processfeed.sh
+++ processfeed.sh
@@ -2,7 +2,19 @@
 
 echo "URL del feed rss"
 read url
+html=""
 wget -nv -cO - $url > feedrss
 linea=$(grep "<item>" feedrss -m 1 -n | cut -f1 -d:)
-sed "1,$linea""d" feedrss | grep -E "<title>|<pubDate>"  | sed -e 's/<title>//g' -e 's/<pubDate>//g' |sed -e 's/<\/title>//g' -e 's/<\/pubDate>//g' | sed 's/<!\[CDATA\[//g' | sed 's/\]\]>//g'
-
+sed "1,$linea""d" feedrss | grep -E "<title>|<pubDate>"  | sed -e 's/<title>//g' -e 's/<pubDate>//g' |sed -e 's/<\/title>//g' -e 's/<\/pubDate>//g' | sed 's/<!\[CDATA\[//g' | sed 's/\]\]>//g' > salida.txt
+let CONT=0
+while IFS='' read -r linea || [[ -n "$linea" ]]; do
+    let CONT++
+    if [[ CONT -eq 2 ]]; then
+    	html+="<span>$linea</span>"
+    	CONT=0
+    else
+    	html+="<h1>$linea</h1>"
+    fi
+done < salida.txt
+rm salida.txt
+echo $html > index.html
